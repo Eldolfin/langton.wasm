3aa0e8f40bb6525717ce2f6bddff5fd426c92b5d
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-06-12 13:19:57 +0200

    feat(debug_ui): started adding style, wip add manual value input

--- lib/debug_ui/src/lib.rs
+++ lib/debug_ui/src/lib.rs
@@ -54,10 +54,18 @@ impl DebugUI {
 
         let root = document.create_element("div").unwrap();
         let title_btn = document.create_element("h2").unwrap();
-        root.append_child(&title_btn).unwrap();
-        title_btn.set_text_content(Some(title));
 
+        title_btn.set_text_content(Some(title));
+        root.set_class_name("DebugUI-root-box");
+        title_btn.set_class_name("DebugUI-title-btn");
+
+        root.append_child(&title_btn).unwrap();
         body.append_child(&root).unwrap();
+
+        let style = document.create_element("style").unwrap();
+        style.set_text_content(Some(include_str!("./style.css")));
+        document.head().unwrap().append_child(&style).unwrap();
+
         Self::Enabled {
             root,
             document,
@@ -65,7 +73,7 @@ impl DebugUI {
         }
     }
 
-    pub fn param<T: ToPrimitive + FromPrimitive + Copy + Default + 'static>(
+    pub fn param<T: ToPrimitive + FromPrimitive + Copy + Default + 'static + ToString>(
         &mut self,
         name: &str,
         default_value: T,
@@ -80,32 +88,62 @@ impl DebugUI {
             } => {
                 let container = document.create_element("div").unwrap();
                 let label = document.create_element("label").unwrap();
-                let slider = document.create_element("input").unwrap();
+                let slider = document
+                    .create_element("input")
+                    .unwrap()
+                    .dyn_into::<HtmlInputElement>()
+                    .unwrap();
+                let value_input = document
+                    .create_element("input")
+                    .unwrap()
+                    .dyn_into::<HtmlInputElement>()
+                    .unwrap();
+
                 let uid = *next_uid;
                 *next_uid += 1;
                 let slider_id = format!("debugui-slider-{uid}");
+                let value_id = format!("debugui-value-{uid}");
 
                 slider.set_id(&slider_id);
+                value_input.set_id(&value_id);
+
                 slider.set_attribute("type", "range").unwrap();
+                value_input.set_attribute("type", "number").unwrap();
                 label.set_text_content(Some(name));
+                label.set_attribute("for", &slider_id).unwrap();
+                value_input.set_value_as_number(default_value.to_f64().unwrap());
+                slider
+                    .set_attribute("min", &range.start.to_string())
+                    .unwrap();
+                slider.set_attribute("max", &range.end.to_string()).unwrap();
 
                 container.set_class_name("DebugUI-param-container");
                 label.set_class_name("DebugUI-param-label");
                 slider.set_class_name("DebugUI-param-slider");
+                value_input.set_class_name("DebugUI-param-value");
 
                 container.append_child(&label).unwrap();
                 container.append_child(&slider).unwrap();
+                container.append_child(&value_input).unwrap();
                 root.append_child(&container).unwrap();
 
                 let document = document.clone();
                 let name = name.to_owned();
-                let on_change = EventListener::new(&slider, "change", move |_event| {
+                let on_change = EventListener::new(&slider, "input", move |_event| {
                     let value = document
                         .get_element_by_id(&slider_id)
                         .unwrap()
                         .dyn_into::<HtmlInputElement>()
                         .unwrap()
                         .value_as_number();
+                    let value_input = document
+                        .get_element_by_id(&value_id)
+                        .unwrap()
+                        .dyn_into::<HtmlInputElement>()
+                        .unwrap();
+
+                    value_input.set_value_as_number(value);
+
                     let value = T::from_f64(value).unwrap_or_else(|| {
                         panic!("Failed to cast slider value for parameter {name}")
                     });
